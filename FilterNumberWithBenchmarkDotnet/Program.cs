accf498c39868f7e54773af49c5a9ffd5ff61fa2
Marcus Tung <marcus_tung@mayohr.com>
2023-01-06 11:59:47 +0800

    加入專案檔案。

---
+++ FilterNumberWithBenchmarkDotnet/Program.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+// See https://aka.ms/new-console-template for more information
+
+var summary = BenchmarkRunner.Run<RegexvsNoRegex>();
+Console.ReadKey();
+
+[ClrJob, CoreJob]
+public class RegexvsNoRegex
+{
+    private string[] strings = { "FMC787", "FMC123", "SUMMARY", "BEBO", "DJN55" };
+
+    [Benchmark]
+    public void Regex()
+    {
+        strings.Select(x => filterNumber(x));
+    }
+
+    [Benchmark]
+    public void NoRegex()
+    {
+        strings.Select(s => s.Substring(0, s.TakeWhile(char.IsLetter).Count()));
+    }
+
+    public string filterNumber(string input)
+    {
+        return System.Text.RegularExpressions.Regex.Match(input, @"[A-Z]+").ToString();
+    }
+}
